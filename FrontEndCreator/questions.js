3a7abefc3cf1476338d9b75926539bbcb3c7d3ec
stephenjoro <stephenjoro@gmail.com>
2014-09-29 12:30:39 -0400

    updated to object parameters

--- FrontEndCreator/questions.js
+++ FrontEndCreator/questions.js
@@ -1,36 +1,37 @@
 //must have a blank option one for question of type dropdown
 
-standard('qTypes',["","Drop Down", "Multidrop-down", "Number Fillin", "Essay", "Code"]);
+standard('qTypes',['','Drop Down', 'Multidrop-down', 'Number Fillin', 'Essay', 'Code']);
 
-standard('qTypes2',["","dropdowns", "multidropdowns", "numfillins", "essays", "codes"]);
-standard('correct',["Correct","Incorrect"]);
-standard('incorrect',["Incorrect","Correct"]);
+standard('qTypes2',['','dropdowns', 'multidropdowns', 'numfillins', 'essays', 'codes']);
+standard('correct',['Correct','Incorrect']);
+standard('incorrect',['Incorrect','Correct']);
 
 
-quiz([
+quiz({qList:[
   
-  question("dropdown","Type of Question:",choices['qTypes'],'Choose a type...',choices['qTypes2']),
-  question("essay","Question Text:",3,30,'Your text here...'),
-  questionSet("dropdowns multidropdowns",[
-    question("essay","Placeholder Text:",3,30,'Your text here...'),
-    question("multiquestion","",[
-      question("essay","Answer Choice 1:",3,30,'Your text here...'),
-      question("dropdown","",choices['correct'])
-    ]),
-    question("multiquestion","",[
-      question("essay","Answer Choice 2:",3,30,'Your text here...'),
-      question("dropdown","",choices['incorrect'])
-    ]),
-    question("multiquestion","",[
-      question("essay","Answer Choice 3:",3,30,'Your text here...'),
-      question("dropdown","",choices['incorrect'])
-    ])
-  ]),
-  questionSet("codes",[
-    question("essay","Base Code:",3,30,'Your base code here...')
-  ]),
-  questionSet("numfillins essays",[
-    question("essay","Placeholder Text:",3,30,'Your placeholder text here...')
-  ])
-]);
-
+  question({type:'dropdown',qHTML:'Type of Question:',ansrL:choices['qTypes'],pHold:'Choose a type...',hideL:choices['qTypes2']}),
+  question({type:'essay',qHTML:'Question Text:',rows:3,cols:30,pHold:'Your text here...'}),
+  questionSet({class:'dropdowns multidropdowns',qList:[
+    question({type:'essay',qHTML:'Placeholder Text:',rows:3,cols:30,pHold:'Your text here...'}),
+    question({type:'multiquestion',qHTML:'',qArry:[
+      question({type:'essay',qHTML:new fromText('Answer Choice [list:nums]:',1),rows:3,cols:30,pHold:'Your text here...'}),
+      question({type:'dropdown',qHTML:'',ansrL:choices['correct']})
+    ]}),
+    question({type:'multiquestion',qHTML:'',qArry:[
+      question({type:'essay',qHTML:new fromText('Answer Choice [list:nums]:',2),rows:3,cols:30,pHold:'Your text here...'}),
+      question({type:'dropdown',qHTML:'',ansrL:choices['incorrect']})
+    ]}),
+    question({type:'multiquestion',qHTML:'',qArry:[
+      question({type:'essay',qHTML:new fromText('Answer Choice [list:nums]:',3),rows:3,cols:30,pHold:'Your text here...'}),
+      question({type:'dropdown',qHTML:'',ansrL:choices['incorrect']})
+    ]})
+  ]}),
+  
+  questionSet({class:'codes',qList:[
+    question({type:'essay',qHTML:'Base Code:',rows:3,cols:30,pHold:'Your base code here...'})
+  ]}),
+  questionSet({class:'numfillins essays',qList:[
+    question({type:'essay',qHTML:'Placeholder Text:',rows:3,cols:30,pHold:'Your placeholder text here...'})
+  ]})
+  
+],opts:{timed:false}});
