ae5e4dd69d17ecc72d839645b79a6b72e18b25ec
stephenjoro <stephenjoro@gmail.com>
2014-10-08 20:32:04 -0400

    updated to better answer searching

--- StudentView/chosenSubmit.js
+++ StudentView/chosenSubmit.js
@@ -5,26 +5,38 @@ for (var selector in config) {
   $(selector).chosen(config[selector]);
 }
 
-
+for(var k = 0; k < showHideList.length; k++){
+  // alert(showHide);
+  var showHide=showHideList[k];
+  for(var i = 0; i < showHide.length; i++){
+  //  alert(showHide[i]);
+    if(showHide[i].length>0)
+      $("."+showHide[i]).hide();
+  }
+}
 
 function SUBMIT_ONE_QUIZ(quiz){
+
   var i = 0;
   var allSend = "";
+  $("*").each(function(){
+    i++;
+    if(!$(this).attr("id"))
+      $(this).attr("id","$"+i);
+  });
+ 
+
   $(quiz).find(".question").each(function(){
-    if(!$(this).hasClass("nonquestion")){
-      if(i>0)
-        allSend+=",\n";
+
+    if($(this).parent().is(':visible')){
       var val = $(this).val();
-      if(val==null){
-        val = "No Answer";
-      }
-      if(val.length < 1){
-        val = "No Answer";
-      }
+
       //BTW we will escape the val of each answer so that students can't mess server parsing up
-      allSend+=("{q"+i+": "+val+"}");
-      i++;
+      if(!(val==null||val.length < 1))
+        allSend+=(allSend.length>0?",\n":"")+("{"+$(this).attr("id")+":"+val+"}");
+      
     }
+
   });
   alert(allSend);
 }
